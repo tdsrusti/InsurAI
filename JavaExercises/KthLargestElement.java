e1b33d645593160c27f587a8df0cc8a38a5a3e66
Srusti T D <srustitd@example.com>
2025-10-27 16:39:54 +0530

    23/10/2025

---
+++ JavaExercises/KthLargestElement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class KthLargestElement {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) {
+            pq.add(n);
+            if (pq.size() > k) pq.poll();
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(findKthLargest(nums, 2)); // 5
+    }
+}
