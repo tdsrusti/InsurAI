69b850df43ac46b33c4e54400bc0563942d7098f
Srusti T D <srustitd@example.com>
2025-10-27 16:33:09 +0530

    21/10/2025

---
+++ JavaExercises/ValidParentheses.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "{[()]}";
+        System.out.println(isValid(s)); // true
+    }
+}
