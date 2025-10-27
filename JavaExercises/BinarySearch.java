caabc6a9764646015c661ed39baabb43f39b4347
Srusti T D <srustitd@example.com>
2025-10-27 16:25:26 +0530

    17/10/2025

---
+++ JavaExercises/BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        System.out.println(search(nums, 9)); // 4
+    }
+}
