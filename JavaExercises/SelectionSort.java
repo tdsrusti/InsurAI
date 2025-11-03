6fba1860c1ae5686ab8004307a62c1a3db297dbb
Srusti T D <srustitd@example.com>
2025-11-03 18:36:32 +0530

    02/11/2025

---
+++ JavaExercises/SelectionSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) minIdx = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
